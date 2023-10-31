3a089fb707319b3326ce3ba0ddff3b0e36e6c6d0
Tollic <72671011+Tollic@users.noreply.github.com>
2023-10-31 19:27:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import websockets
+
+# Список подключенных клиентов
+connected_clients = set()
+
+async def connect_client(websocket, path):
+    # Добавление клиента в список подключенных
+    connected_clients.add(websocket)
+    try:
+        async for message in websocket:
+            # Распространение сообщения по всем подключенным клиентам
+            for client in connected_clients:
+                await client.send(message)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+    finally:
+        # Удаление клиента из списка, если он отключился
+        connected_clients.remove(websocket)
+
+start_server = websockets.serve(connect_client, "localhost", 8765)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
